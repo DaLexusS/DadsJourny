9faf395a1d50d1e2c2aea59122db285b09f69508
benpanski <benpanski@gmail.com>
2025-01-27 17:20:03 +0200

    spike death bug fix

--- Assets/Scripts/Traps/DeathOnTouch.cs
+++ Assets/Scripts/Traps/DeathOnTouch.cs
@@ -6,7 +6,8 @@ public class DeathOnTouch : MonoBehaviour
     static public UnityAction onPlayerDeath;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!collision.gameObject.CompareTag("PlayerCharacter")) { Debug.Log("Was collided but not with PlayerCharacter"); return; }
+        
+        if (!collision.gameObject.CompareTag("PlayerCharacter")) { Debug.Log("Was collided with " + collision.gameObject.name + " but not with PlayerCharacter"); return; }
         Debug.Log("collided with player, invoking death");
         onPlayerDeath.Invoke();
     }
